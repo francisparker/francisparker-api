112d6ed33e629e3e35c976d491b40075d97425ed
RIGIK93 <maxim.peniaz@gmail.com>
2022-09-15 23:41:54 -0400

    Fix: Crash on video instead of image. New function added

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Francis Parker Public API
-Hello, this is Francis Parker Public API. It's used to access data on [francis parker website](https://francisparkelouisville.org) and other related to francis parker resources
+Hello, this is Francis Parker Public API. It's used to access data on [francis parker website](https://francisparkerlouisville.org) and other related to francis parker resources
+# installation
+```
+$ npm install francisparker-api
+```
 # API's
 ## News API
 News API is used to fetch news from [Wyvern Bulletin Board](https://francisparkerlouisville.org/wyvernbullbtinboard/)
@@ -19,7 +23,7 @@ async function main() {
 main() 
 ```
 
-### Scrap The Whole Page
+### Fetch The Whole Page
 You can get a list of articles at a certain page
 ```typescript
 // Import function that fetches news
@@ -27,11 +31,28 @@ import { fetchPage } from 'francisparker-api/news'
 // Import Interface for typing (optional)
 import type { Article } from 'francisparker-api/news'
 
-// fetch article
+// fetch articles
 async function main() {
     const pageNumber: number = 0 // page numbers are integers [0, last page], where 0 is the first page (page with the latest news)
     const articles: Article[] = await fetchPage(pageNumber)
 }
 
 main() 
+```
+
+### Fetch A Specific Article
+You can fetch a specific article by number. Starting from 0 (the newest article) to max (the oldest article)
+```typescript
+// Import function that fetches news
+import { fetchArticle } from 'francisparker-api/news'
+// Import Interface for typing (optional)
+import type { Article } from 'francisparker-api/news'
+
+// fetch articles
+async function main() {
+    const articleNumber: number = 0 // article numbers are integers [0, last article], where 0 is the latest article and last article is the oldest one
+    const articles: Article[] = await fetchPage(pageNumber)
+}
+
+main()
 ```
--- news/index.ts
+++ news/index.ts
@@ -28,49 +28,83 @@ export async function fetchPage(pageNumber: number): Promise<Article[]> {
     const arr: Article[] = [];
     const webPage = await axios.get(pageToFetch(pageNumber));
     if (webPage.status != 200) {
-        throw new Error(`Error: While loading '${fetchFrom}' returned wrong status code. Status code: '${webPage.status}'`)
+        throw new Error(`Error: While loading '${pageToFetch(pageNumber)}' returned wrong status code. Status code: '${webPage.status}'`)
     }
     const $ = load(webPage.data)
     const raw = $("article");
-    let curr;
-    raw.toArray().map((element, index) => {
+    if (!raw) throw new Error("No articles found")
+    await Promise.all(raw.toArray().map(async (element, index) => {
         // console.log("\n\n" + load(element).html() + "\n\n")
-        arr.push(processArticle(load(element).html()))
-    })
-    // if (!raw) throw new Error("No articles found")
-        // return processArticle(raw);
+        arr.push(await processArticle(load(element).html()))
+    }))
     return arr;
 }
 
-function processArticle(raw_article: string): Article {
+/** fetch a specific article (0 is latest) */
+export async function fetchArticle(articleNumber: number) {
+    const articlesOnPage = 6;
+    const articleIndex = articleNumber % articlesOnPage
+    const pageNumber = (articleNumber - articleIndex) / articlesOnPage;
+    // console.log(pageNumber)
+    const webPage = await axios.get(pageToFetch(pageNumber));
+    if (webPage.status != 200) {
+        throw new Error(`Error: While loading '${pageToFetch(pageNumber)}' returned wrong status code. Status code: '${webPage.status}'`)
+    }
+    const $ = load(webPage.data)
+    const raw = $("article");
+    if (!raw) throw new Error("No articles found")
+    // await Promise.all(raw.toArray().map(async (element, index) => {
+    //     // console.log("\n\n" + load(element).html() + "\n\n")
+    //     arr.push(await processArticle(load(element).html()))
+    // }))
+    // return arr;
+    return await processArticle(load(raw.toArray()[articleIndex]).html())
+}
+
+async function processArticle(raw_article: string): Promise<Article> {
+    // console.log(raw_article)
     const $ = load(raw_article);
 
     const img = $('div:first>div:first>ul:first>li:first>div:first>a:first>img:first')
-    
+    const iframe = $('div:first>div:first>ul:first>li:first>div:first>iframe:first')
+    // console.log(typeof (iframe.toArray())[0])
+
     const img_src = img.attr("src")
+    const iframe_src = iframe.attr("src")
+    // console.log(img_src)
+    // console.log(iframe_src)
 
     // if (!img_src) throw new Error("Image on the webpage doesn't have an src")
     // BUG! For some elements (ex. p12) there is a video instead of image, so everything fails
-    const title_raw = img_src ? $('div:first>div:nth-child(2)>div:first>h2:first>a:first') : $('div:first>div:first>div:first>h2:first>a:first')
-    
+    const title_raw = (img_src || iframe_src) ? $('div:first>div:nth-child(2)>div:first>h2:first>a:first') : $('div:first>div:first>div:first>h2:first>a:first')
     const link_to_article = title_raw.attr('href')
-    // if (!link_to_article) throw new Error("Article doesn't have a link")
+    // console.log()
+    // console.log(img_src)
+    // console.log(iframe_src)
+    // console.log(link_to_article)
+    if (!link_to_article) throw new Error("Article doesn't have a link")
 
     const title = title_raw.text();
     // BUG! For some elements (ex. p12) there is a video instead of image, so everything fails
-    const content = img_src ? $('div:first>div:nth-child(2)>div:first>div:last>p').text() : $('div:first>div:first>div:first>div:last>p').text()
+    const description = (img_src || iframe_src) ? $('div:first>div:nth-child(2)>div:first>div:last>p').text() : $('div:first>div:first>div:first>div:last>p').text()
 
     // return {sender: "**Francis Parker**", title: title, content: `${content}\n[Read more...](${link_to_article})`, files: [{attachment: img_src}]}
     return {
-        image: img_src ? {
+        banner: img_src ? {
             element: img,
-            src: img_src ? img_src : ''
-        } : undefined,
-        link: link_to_article ? link_to_article : '',
-        content: content,
-        title: title
+            src: img_src
+        } as Image : (iframe_src ? {src: iframe_src} as Video : undefined),
+        link: link_to_article,
+        description: description,
+        title: title,
+        content: await getArticleContent(link_to_article)
     }
     
+}
+
+async function getArticleContent(url: string): Promise<Cheerio<CElement>> {
+    const $ = load(await axios.get(url))
+    return $(".post-content:first")
 }
 
 export interface Image {
@@ -80,16 +114,31 @@ export interface Image {
     src: string
 }
 
+export interface Video {
+    /** physical link to the video */
+    src: string;
+}
+
+//** Short News Article */
 export interface Article {
-    /** image of the banner of the article */
-    image: Image | undefined
+    /** Media of the banner of the article */
+    banner?: Image | Video
     /** Link to the "Read More" */
     link: string
     /** Title of the article */
     title: string
     /** Content of the article */
-    content: string
+    description: string
+
+    content: Cheerio<CElement>
 }
+
+// export interface ArticleContent {
+//     bannner?: Image
+//     title: string
+//     content: Cheerio<CElement>;
+// }
+
 
 // fetchLatest().then((result) => {
 //     // console.log(`Done: ${JSON.stringify(result)}`)
@@ -102,13 +151,14 @@ export interface Article {
 //         console.log("err! " + err)
 // });
 // async function main() {
-//     for (let i = 0; i < 50; i++) {
-//         console.log("Page: " + i)
-//         // fetchPage(i).then((result) => {
-//             for (const { title } of await fetchPage(i)) {
-//                 console.log(title);
-//             }
-//         // })
-//     }
+//     // for (let i = 0; i < 50; i++) {
+//     //     console.log("Page: " + i)
+//     //     // fetchPage(i).then((result) => {
+//     //     for (const { title } of await fetchPage(i)) {
+//     //             console.log(title);
+//     //     }
+//     //     // })
+//     // }
+//     // console.log((await fetchArticle((14 * 6) + 4)).description)
 // }
 // main() 
